deb83a33782bc6770c47d4d54a86c12141d9e473
jonma1221 <jonma1221@gmail.com>
2016-02-24 22:28:22 -0800

    initial

---
+++ JNotepad/src/JNotepad.java
@@ -0,0 +1,26 @@
+//
+// Name: Ma, Jonathan
+// Project: #4
+// Due: 12/4/15
+// Course: CS-245-01-f15
+//
+// Description:
+// A text editor that inplements some of Window's notepad features.
+//
+
+
+/**
+ *
+ * @author Jonathan
+ */
+public class JNotepad{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frame fr = new frame();
+        fr.setVisible();
+    }
+    
+}
